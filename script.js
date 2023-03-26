6c0482ce3dc36f16e749840ed538fe54647c238b
Mwai Banda <bandamwai@gmail.com>
2023-03-26 04:41:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function playGuessingGame(numToGuess, totalGuesses = 10) {
+  let userGuess;
+  let numberOfTrys = 0
+  do {
+    userGuess = prompt("Enter a number between 1 and 100.")
+    console.log(`user guess ${userGuess} - ${isNaN(userGuess)}`)
+    numberOfTrys++
+  } while(
+    (userGuess < 1 || userGuess > 100) && numberOfTrys < totalGuesses
+  )
+  return 0 
+}
