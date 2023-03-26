74c3340eaaf754a0c7874359cfdb9063aff05cd0
Mwai Banda <bandamwai@gmail.com>
2023-03-26 05:56:44 +0000

    Finalized guessing game functionality

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 function playGuessingGame(numToGuess, totalGuesses = 10) {
   let userGuess;
   let numberOfTrys = 0
+  let promptLabel = "Enter a number between 1 and 100."
+
   do {
-    userGuess = prompt("Enter a number between 1 and 100.")
-    console.log(`user guess ${userGuess} - ${isNaN(userGuess)}`)
-    numberOfTrys++
+    userGuess = prompt(promptLabel)
+    if (userGuess === null) {
+      return 0 
+    }
+    let userGuessNumber = parseInt(userGuess)
+    if(userGuessNumber === numToGuess) {
+      numberOfTrys++
+      return numberOfTrys
+    } else if(userGuessNumber < numToGuess) {
+      promptLabel = `${userGuess} is too small. Guess a larger number.`
+      numberOfTrys++
+    } else if (userGuessNumber > numToGuess) {
+      promptLabel = `${userGuess} is too large. Guess a smaller number.`
+      numberOfTrys++
+    } else if (userGuessNumber === "" || isNaN(userGuess)) {
+      promptLabel = "Please enter a number."
+      
+    }
+    // console.log(`user guess ${userGuess} - ${isNaN(userGuess)}`)
   } while(
-    (userGuess < 1 || userGuess > 100) && numberOfTrys < totalGuesses
+    userGuess < 1       || 
+    userGuess > 100     || 
+    numberOfTrys === "" || 
+    isNaN(userGuess)    ||
+    numberOfTrys < totalGuesses 
   )
+  // console.log(`numberOfTrys - ${numberOfTrys} \ntotalGuesses - ${totalGuesses}`)
   return 0 
 }
